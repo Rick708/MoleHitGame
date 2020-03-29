a5e426a3ee217003bc3cf8a36408d018f6c375bb
Rick708 <Rick708@users.noreply.github.com>
2020-03-30 06:27:45 +0900

    score

--- Assets/Script/Game/TimeCunter.cs
+++ Assets/Script/Game/TimeCunter.cs
@@ -7,13 +7,34 @@ public class TimeCunter : MonoBehaviour
 {
     public float countdown = 5.0f;
     public Text timeText;
+    public Text Score;
     private bool isPose = false;
     public GameObject StartButton;
+
+    int myScore = 0;
 
     public void OnStartButton()
     {
         StartTime();
     }
+
+    private void Update()
+    {
+        Score.text = "score:" + myScore;
+    }
+
+    public void AddScore(int add)
+    {
+        if(countdown > 0)
+        {
+            myScore += add;
+        }
+    }
+
+
+
+
+
     void StartTime()
     {
         countdown -= Time.deltaTime;
