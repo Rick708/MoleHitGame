622a71964831239c364b17bf87992a201455a690
Rick708 <Rick708@users.noreply.github.com>
2020-03-29 22:02:19 +0900

    image追加

---
+++ Assets/Script/Game/MoleManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class MoleManager : MonoBehaviour
+{
+    Action tapAction;
+
+    public void AddEventListenerOnTap(Action action)
+    {
+        tapAction += action;
+    }
+
+    public void OnTap()
+    {
+        tapAction();
+    }
+}
